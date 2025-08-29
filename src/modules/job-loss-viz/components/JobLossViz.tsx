d2d9af3a0a7da209b4299c2b21851907f2940384
Darkness <davidskg10@gmail.com>
2025-08-29 17:56:42 +0800

    Update JobLossViz.tsx

--- src/modules/job-loss-viz/components/JobLossViz.tsx
+++ src/modules/job-loss-viz/components/JobLossViz.tsx
@@ -56,8 +56,9 @@ export default function JobLossViz({ className, year = 2025 }: Props) {
             <div className="text-sm text-error">Failed to load data: {error}</div>
           ) : (
             <>
-              <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-4">
-                <div className="md:col-span-2">
+              <div className="flex flex-col gap-6 mt-4">
+                {/* LineGraph Card */}
+                <div className="card-base p-6 relative">
                   <LineGraph data={ytdSeries} height={200} />
                   
                   {/* Total YTD Value */}
@@ -69,42 +70,34 @@ export default function JobLossViz({ className, year = 2025 }: Props) {
                       </div>
                     </div>
                   )}
+                  
+                  {/* Floating Action Button for Sources - positioned within LineGraph card */}
+                  <FloatingActionButton 
+                    onClick={handleOpenSourceModal} 
+                    ariaLabel="View data sources"
+                    position="bottom-right"
+                    className="absolute bottom-4 right-4"
+                    size="sm"
+                    icon={
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+                      </svg>
+                    }
+                  />
                 </div>
                 
-                {/* Industry Breakdown */}
-                <div>
-                  {industries && industries.length > 0 && (
+                {/* Industry Breakdown Card */}
+                {industries && industries.length > 0 && (
+                  <div className="card-base p-6">
                     <IndustryList industries={industries} maxItems={5} />
-                  )}
-                </div>
+                  </div>
+                )}
               </div>
               
-              {/* Floating Action Button for Sources */}
-              <FloatingActionButton 
-                onClick={handleOpenSourceModal} 
-                ariaLabel="View data sources"
-                position="bottom-right"
-                className="absolute"
-                size="sm"
-                icon={
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path fillRule="evenodd" d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-                  </svg>
-                }
-              />
             </>
           )}
         </div>
         
-        {/* Sources Panel (Separate) */}
-        {!error && latestSources && latestSources.length > 0 && (
-          <div className="mt-6 card-base p-6">
-            <SourcePanel 
-              sources={latestSources} 
-              className="w-full" 
-            />
-          </div>
-        )}
         
         {/* Sources Modal */}
         <InfoModal 
@@ -123,6 +116,7 @@ export default function JobLossViz({ className, year = 2025 }: Props) {
             }
             isModal={true} 
             onClose={handleCloseSourceModal} 
+            className="w-full"
           />
         </InfoModal>
       </div>
